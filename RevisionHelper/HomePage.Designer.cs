7270474d9b5a6113ab23b09a6084ae84dc226cc9
Connor Fone <connor.g.fone@gmail.com>
2024-11-22 13:55:47 +0000

    Created a prototype Homepage UI

---
+++ RevisionHelper/HomePage.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace RevisionHelper
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTNmyProgress = new System.Windows.Forms.Button();
+            this.BTNtopics = new System.Windows.Forms.Button();
+            this.BTNquestions = new System.Windows.Forms.Button();
+            this.BTNpractice = new System.Windows.Forms.Button();
+            this.BTNcreateQuestions = new System.Windows.Forms.Button();
+            this.BTNeditQuestions = new System.Windows.Forms.Button();
+            this.BTNrevisionSchedule = new System.Windows.Forms.Button();
+            this.BTNeditProfile = new System.Windows.Forms.Button();
+            this.BTNlogout = new System.Windows.Forms.Button();
+            this.LBLrecentTopics = new System.Windows.Forms.Label();
+            this.LBLtopic1Desc = new System.Windows.Forms.Label();
+            this.LBLtopic1name = new System.Windows.Forms.Label();
+            this.PBtopic1 = new System.Windows.Forms.PictureBox();
+            this.BTNpracticeTopic1 = new System.Windows.Forms.Button();
+            this.PNLtopic1 = new System.Windows.Forms.Panel();
+            this.PNLtopic2 = new System.Windows.Forms.Panel();
+            this.BTNpracticeTopic2 = new System.Windows.Forms.Button();
+            this.LBLtopic2Desc = new System.Windows.Forms.Label();
+            this.LBLtopic2name = new System.Windows.Forms.Label();
+            this.PBtopic2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBtopic1)).BeginInit();
+            this.PNLtopic1.SuspendLayout();
+            this.PNLtopic2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBtopic2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTNmyProgress
+            // 
+            this.BTNmyProgress.Location = new System.Drawing.Point(12, 12);
+            this.BTNmyProgress.Name = "BTNmyProgress";
+            this.BTNmyProgress.Size = new System.Drawing.Size(160, 49);
+            this.BTNmyProgress.TabIndex = 0;
+            this.BTNmyProgress.Text = "My Progress";
+            this.BTNmyProgress.UseVisualStyleBackColor = true;
+            // 
+            // BTNtopics
+            // 
+            this.BTNtopics.Location = new System.Drawing.Point(12, 67);
+            this.BTNtopics.Name = "BTNtopics";
+            this.BTNtopics.Size = new System.Drawing.Size(160, 49);
+            this.BTNtopics.TabIndex = 1;
+            this.BTNtopics.Text = "Topics";
+            this.BTNtopics.UseVisualStyleBackColor = true;
+            // 
+            // BTNquestions
+            // 
+            this.BTNquestions.Location = new System.Drawing.Point(12, 122);
+            this.BTNquestions.Name = "BTNquestions";
+            this.BTNquestions.Size = new System.Drawing.Size(160, 49);
+            this.BTNquestions.TabIndex = 2;
+            this.BTNquestions.Text = "Questions";
+            this.BTNquestions.UseVisualStyleBackColor = true;
+            // 
+            // BTNpractice
+            // 
+            this.BTNpractice.Location = new System.Drawing.Point(12, 177);
+            this.BTNpractice.Name = "BTNpractice";
+            this.BTNpractice.Size = new System.Drawing.Size(160, 49);
+            this.BTNpractice.TabIndex = 3;
+            this.BTNpractice.Text = "Practice";
+            this.BTNpractice.UseVisualStyleBackColor = true;
+            // 
+            // BTNcreateQuestions
+            // 
+            this.BTNcreateQuestions.Location = new System.Drawing.Point(12, 232);
+            this.BTNcreateQuestions.Name = "BTNcreateQuestions";
+            this.BTNcreateQuestions.Size = new System.Drawing.Size(160, 49);
+            this.BTNcreateQuestions.TabIndex = 4;
+            this.BTNcreateQuestions.Text = "Create Question";
+            this.BTNcreateQuestions.UseVisualStyleBackColor = true;
+            // 
+            // BTNeditQuestions
+            // 
+            this.BTNeditQuestions.Location = new System.Drawing.Point(12, 287);
+            this.BTNeditQuestions.Name = "BTNeditQuestions";
+            this.BTNeditQuestions.Size = new System.Drawing.Size(160, 49);
+            this.BTNeditQuestions.TabIndex = 5;
+            this.BTNeditQuestions.Text = "Edit My Questions";
+            this.BTNeditQuestions.UseVisualStyleBackColor = true;
+            // 
+            // BTNrevisionSchedule
+            // 
+            this.BTNrevisionSchedule.Location = new System.Drawing.Point(12, 342);
+            this.BTNrevisionSchedule.Name = "BTNrevisionSchedule";
+            this.BTNrevisionSchedule.Size = new System.Drawing.Size(160, 49);
+            this.BTNrevisionSchedule.TabIndex = 6;
+            this.BTNrevisionSchedule.Text = "Revision Schedule";
+            this.BTNrevisionSchedule.UseVisualStyleBackColor = true;
+            // 
+            // BTNeditProfile
+            // 
+            this.BTNeditProfile.Location = new System.Drawing.Point(12, 397);
+            this.BTNeditProfile.Name = "BTNeditProfile";
+            this.BTNeditProfile.Size = new System.Drawing.Size(160, 49);
+            this.BTNeditProfile.TabIndex = 7;
+            this.BTNeditProfile.Text = "Edit Profile";
+            this.BTNeditProfile.UseVisualStyleBackColor = true;
+            // 
+            // BTNlogout
+            // 
+            this.BTNlogout.Location = new System.Drawing.Point(12, 452);
+            this.BTNlogout.Name = "BTNlogout";
+            this.BTNlogout.Size = new System.Drawing.Size(160, 49);
+            this.BTNlogout.TabIndex = 8;
+            this.BTNlogout.Text = "Topics";
+            this.BTNlogout.UseVisualStyleBackColor = true;
+            // 
+            // LBLrecentTopics
+            // 
+            this.LBLrecentTopics.AutoSize = true;
+            this.LBLrecentTopics.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLrecentTopics.Location = new System.Drawing.Point(178, 19);
+            this.LBLrecentTopics.Name = "LBLrecentTopics";
+            this.LBLrecentTopics.Size = new System.Drawing.Size(197, 31);
+            this.LBLrecentTopics.TabIndex = 9;
+            this.LBLrecentTopics.Text = "Recent Topics:";
+            // 
+            // LBLtopic1Desc
+            // 
+            this.LBLtopic1Desc.AutoSize = true;
+            this.LBLtopic1Desc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLtopic1Desc.Location = new System.Drawing.Point(28, 205);
+            this.LBLtopic1Desc.Name = "LBLtopic1Desc";
+            this.LBLtopic1Desc.Size = new System.Drawing.Size(51, 20);
+            this.LBLtopic1Desc.TabIndex = 3;
+            this.LBLtopic1Desc.Text = "label1";
+            // 
+            // LBLtopic1name
+            // 
+            this.LBLtopic1name.AutoSize = true;
+            this.LBLtopic1name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLtopic1name.Location = new System.Drawing.Point(28, 18);
+            this.LBLtopic1name.Name = "LBLtopic1name";
+            this.LBLtopic1name.Size = new System.Drawing.Size(116, 20);
+            this.LBLtopic1name.TabIndex = 2;
+            this.LBLtopic1name.Text = "Recent Topic 1";
+            // 
+            // PBtopic1
+            // 
+            this.PBtopic1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.PBtopic1.Location = new System.Drawing.Point(32, 41);
+            this.PBtopic1.Name = "PBtopic1";
+            this.PBtopic1.Size = new System.Drawing.Size(224, 161);
+            this.PBtopic1.TabIndex = 1;
+            this.PBtopic1.TabStop = false;
+            // 
+            // BTNpracticeTopic1
+            // 
+            this.BTNpracticeTopic1.Location = new System.Drawing.Point(181, 288);
+            this.BTNpracticeTopic1.Name = "BTNpracticeTopic1";
+            this.BTNpracticeTopic1.Size = new System.Drawing.Size(75, 23);
+            this.BTNpracticeTopic1.TabIndex = 0;
+            this.BTNpracticeTopic1.Text = "Practice";
+            this.BTNpracticeTopic1.UseVisualStyleBackColor = true;
+            // 
+            // PNLtopic1
+            // 
+            this.PNLtopic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PNLtopic1.Controls.Add(this.BTNpracticeTopic1);
+            this.PNLtopic1.Controls.Add(this.LBLtopic1Desc);
+            this.PNLtopic1.Controls.Add(this.LBLtopic1name);
+            this.PNLtopic1.Controls.Add(this.PBtopic1);
+            this.PNLtopic1.Location = new System.Drawing.Point(193, 67);
+            this.PNLtopic1.Name = "PNLtopic1";
+            this.PNLtopic1.Size = new System.Drawing.Size(286, 358);
+            this.PNLtopic1.TabIndex = 11;
+            // 
+            // PNLtopic2
+            // 
+            this.PNLtopic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PNLtopic2.Controls.Add(this.BTNpracticeTopic2);
+            this.PNLtopic2.Controls.Add(this.LBLtopic2Desc);
+            this.PNLtopic2.Controls.Add(this.LBLtopic2name);
+            this.PNLtopic2.Controls.Add(this.PBtopic2);
+            this.PNLtopic2.Location = new System.Drawing.Point(485, 67);
+            this.PNLtopic2.Name = "PNLtopic2";
+            this.PNLtopic2.Size = new System.Drawing.Size(286, 358);
+            this.PNLtopic2.TabIndex = 12;
+            // 
+            // BTNpracticeTopic2
+            // 
+            this.BTNpracticeTopic2.Location = new System.Drawing.Point(181, 288);
+            this.BTNpracticeTopic2.Name = "BTNpracticeTopic2";
+            this.BTNpracticeTopic2.Size = new System.Drawing.Size(75, 23);
+            this.BTNpracticeTopic2.TabIndex = 0;
+            this.BTNpracticeTopic2.Text = "Practice";
+            this.BTNpracticeTopic2.UseVisualStyleBackColor = true;
+            // 
+            // LBLtopic2Desc
+            // 
+            this.LBLtopic2Desc.AutoSize = true;
+            this.LBLtopic2Desc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLtopic2Desc.Location = new System.Drawing.Point(28, 205);
+            this.LBLtopic2Desc.Name = "LBLtopic2Desc";
+            this.LBLtopic2Desc.Size = new System.Drawing.Size(51, 20);
+            this.LBLtopic2Desc.TabIndex = 3;
+            this.LBLtopic2Desc.Text = "label1";
+            // 
+            // LBLtopic2name
+            // 
+            this.LBLtopic2name.AutoSize = true;
+            this.LBLtopic2name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLtopic2name.Location = new System.Drawing.Point(28, 18);
+            this.LBLtopic2name.Name = "LBLtopic2name";
+            this.LBLtopic2name.Size = new System.Drawing.Size(116, 20);
+            this.LBLtopic2name.TabIndex = 2;
+            this.LBLtopic2name.Text = "Recent Topic 2";
+            // 
+            // PBtopic2
+            // 
+            this.PBtopic2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.PBtopic2.Location = new System.Drawing.Point(32, 41);
+            this.PBtopic2.Name = "PBtopic2";
+            this.PBtopic2.Size = new System.Drawing.Size(224, 161);
+            this.PBtopic2.TabIndex = 1;
+            this.PBtopic2.TabStop = false;
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 504);
+            this.Controls.Add(this.PNLtopic2);
+            this.Controls.Add(this.PNLtopic1);
+            this.Controls.Add(this.LBLrecentTopics);
+            this.Controls.Add(this.BTNlogout);
+            this.Controls.Add(this.BTNeditProfile);
+            this.Controls.Add(this.BTNrevisionSchedule);
+            this.Controls.Add(this.BTNeditQuestions);
+            this.Controls.Add(this.BTNcreateQuestions);
+            this.Controls.Add(this.BTNpractice);
+            this.Controls.Add(this.BTNquestions);
+            this.Controls.Add(this.BTNtopics);
+            this.Controls.Add(this.BTNmyProgress);
+            this.Name = "HomePage";
+            this.Text = "Revision Helper";
+            ((System.ComponentModel.ISupportInitialize)(this.PBtopic1)).EndInit();
+            this.PNLtopic1.ResumeLayout(false);
+            this.PNLtopic1.PerformLayout();
+            this.PNLtopic2.ResumeLayout(false);
+            this.PNLtopic2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBtopic2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNmyProgress;
+        private System.Windows.Forms.Button BTNtopics;
+        private System.Windows.Forms.Button BTNquestions;
+        private System.Windows.Forms.Button BTNpractice;
+        private System.Windows.Forms.Button BTNcreateQuestions;
+        private System.Windows.Forms.Button BTNeditQuestions;
+        private System.Windows.Forms.Button BTNrevisionSchedule;
+        private System.Windows.Forms.Button BTNeditProfile;
+        private System.Windows.Forms.Button BTNlogout;
+        private System.Windows.Forms.Label LBLrecentTopics;
+        private System.Windows.Forms.PictureBox PBtopic1;
+        private System.Windows.Forms.Button BTNpracticeTopic1;
+        private System.Windows.Forms.Label LBLtopic1name;
+        private System.Windows.Forms.Label LBLtopic1Desc;
+        private System.Windows.Forms.Panel PNLtopic1;
+        private System.Windows.Forms.Panel PNLtopic2;
+        private System.Windows.Forms.Button BTNpracticeTopic2;
+        private System.Windows.Forms.Label LBLtopic2Desc;
+        private System.Windows.Forms.Label LBLtopic2name;
+        private System.Windows.Forms.PictureBox PBtopic2;
+    }
+}
+
